5d6ab3206f41885cf7c45a9901adeb4be493a4f8
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-07-21 13:07:57 +0100

    Imports Modal into App Vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
+  <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
       title: 'My First Vue App :)'
